ceadcc31ef1b8b9da59aac1d42268ec5e019254a
William Rodriguez <william@rodriguez1.com>
2022-03-21 14:18:09 -0700

    Add alerts and text to click to show normal and encoded sentence
    Co-Authored-by: Mike Wright <m.wright92@icloud.com>

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-const sentence = prompt("Enter a sentence: ");
-console.log(sentence);
+$(document).ready(function() {
+  const sentence = prompt("Enter a sentence: ");
 
-function getSentenceCaps(sentence) {
-  sentence = sentence.trim();
-  const firstLetter = sentence.slice(0, 1).toUpperCase();
-  const lastLetter = sentence.slice(sentence.length - 1, sentence.length).toUpperCase();
+  function getSentenceCaps(sentence) {
+    sentence = sentence.trim();
+    const firstLetter = sentence.slice(0, 1).toUpperCase();
+    const lastLetter = sentence.slice(sentence.length - 1, sentence.length).toUpperCase();
 
-  return firstLetter + lastLetter;
-}
+    return firstLetter + lastLetter;
+  }
 
-function reverseLetters(letters) {
-  const firstLetter = letters.slice(0, 1);
-  const secondLetter = letters.slice(1, 2);
+  function reverseLetters(letters) {
+    const firstLetter = letters.slice(0, 1);
+    const secondLetter = letters.slice(1, 2);
 
-  return secondLetter + firstLetter;
-}
+    return secondLetter + firstLetter;
+  }
 
-function getMiddleLetter(sentence) {
-  const letterIndex = sentence.length / 2;
-  const middleLetter = sentence.charAt(letterIndex);
-  return middleLetter;
-}
+  function getMiddleLetter(sentence) {
+    const letterIndex = sentence.length / 2;
+    const middleLetter = sentence.charAt(letterIndex);
+    return middleLetter;
+  }
 
-function encode(sentence) {
-  let letters = getSentenceCaps(sentence);
-  letters = reverseLetters(letters);
-  const middleLetter = getMiddleLetter(sentence);
+  function encode(sentence) {
+    let letters = getSentenceCaps(sentence);
+    letters = reverseLetters(letters);
+    const middleLetter = getMiddleLetter(sentence);
 
-  return middleLetter + sentence + letters;
-}
+    return middleLetter + sentence + letters;
+  }
 
-console.log(encode(sentence));
+  const encodedSentence = encode(sentence);
+
+
+
+  $("#original").click(function() {
+    alert(sentence);
+  });
+
+  $("#encode").click(function(){
+    alert(encodedSentence);
+  });
+});
